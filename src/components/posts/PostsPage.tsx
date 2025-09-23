534ef2c3ac045f72ceb5c98c291fbc276bfc8bb9
pawankalyan <pawankalyankarri22@gmail.com>
2025-09-23 19:12:55 +0530

    implementd crud operation

--- src/components/posts/PostsPage.tsx
+++ src/components/posts/PostsPage.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
 
 export type PostType = {
-    userId: number
+    userId: number|undefined
     id: number
     title: string
     body: string
@@ -13,20 +13,31 @@ const PostsPage = () => {
     const [posts,setPosts] = useState<PostType[]>([])
 
     useEffect(()=>{
-        axios.get("https://jsonplaceholder.typicode.com/posts").then(res=>setPosts(res.data)).catch(err=>console.log(err))
-    })
+        
+        axios.get("http://127.0.0.1:8000/posts/getposts/").then(res=>setPosts(res.data)).catch(err=>console.log(err))
+    },[])
+
+    function handleDelete(id:number){
+        setPosts(posts.filter(item=>item.id!== id))
+        axios.delete(`http://127.0.0.1:8000/posts/delete/${id}/`).then(res=>console.log(res)).catch(err=>console.log(err))
+    }
+
     return(
         <div className="flex items-center justify-center flex-col">
-            {posts.map(item=>(
+            <button className="px-3 my-2 p-1 bg-blue-800 text-white font-bold  rounded"><NavLink to="/newpost">Insert Post</NavLink></button>
+            {posts.length === 0 ? <div className="flex justify-center items-center ">Loading Data...</div> :
+            posts.map(item=>(
                 <div key={item.id} className=" m-1 w-[95%]  border-2 p-2 grid grid-cols-[1fr_100px]  ">
+                    
                     <div>
                         <p><strong>id :</strong>&nbsp;{item.id}</p>
+                        <p><strong>userId :</strong>&nbsp;{item.userId}</p>
                         <p><strong>title : </strong>&nbsp;{item.title}</p>
                         <p><strong>body : </strong>&nbsp;{item.body}</p>
                     </div>
                     <div className="flex flex-col justify-center items-center gap-2">
                         <button className="px-3 p-1 bg-green-400 text-white font-bold rounded"><NavLink to={`/posts/${item.id}`}>Edit</NavLink></button>
-                        <button className="px-3 p-1 bg-red-500 text-white font-bold rounded">delete</button>
+                        <button className="px-3 p-1 bg-red-500 text-white font-bold rounded" onClick={()=>handleDelete(item.id)}>delete</button>
                     </div>
                 </div>
 
--- src/components/posts/UpdatePost.tsx
+++ src/components/posts/UpdatePost.tsx
@@ -1,24 +1,103 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import type { PostType } from "./PostsPage";
 import { useEffect, useState } from "react";
 import axios from "axios";
 const UpdatePost = () => {
-    const params = useParams()
-    let [post,setPost] = useState<PostType| undefined>()
+  const params = useParams();
+  let [post, setPost] = useState<PostType>({
+    userId: 0,
+    id: 0,
+    title: "",
+    body: "",
+  });
+  let navigate = useNavigate()
 
+  useEffect(() => {
+    axios
+      .get(`http://127.0.0.1:8000/posts/getpost/${params.id}/`)
+      .then((res) => setPost(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    useEffect(()=>{
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`).then(res=>setPost(res.data)).catch(err=>console.log(err))
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let { name, value } = e.target;
+    setPost((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
-    },[])
-    console.log(post)    
-    return(
-        <div>
-            {post && <div>
-                <p>{post.id}</p>
-                </div>}
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(post);
+    axios.put(
+      `http://127.0.0.1:8000/posts/update/${params.id}/`,
+      post
+    ).then(res=>console.log(res)).catch(err=>console.log(err));
+    navigate('/posts')
+  }
+  return (
+    <div className="flex w-full h-full justify-center items-center font-mono">
+      {post && (
+        <div className="border-2 w-[30%] h-[55%] p-5 rounded-2xl">
+          <form
+            action=""
+            className="flex w-full flex-col gap-2"
+            onSubmit={handleSubmit}
+          >
+            <div className="gap-2 w-full flex">
+              <label htmlFor="">UserId</label>
+              <input
+                className="outline-1 rounded w-full h-fit font-bold text-lg px-2"
+                name="userId"
+                type="number"
+                value={post.userId}
+                onChange={handleChange}
+              />
             </div>
-    )
-}
+            <div className="gap-2 flex">
+              <label htmlFor="">Id</label>
+              <input
+                className="outline-1 rounded w-full h-fit font-bold text-lg px-2"
+                name="id"
+                type="number"
+                value={post.id}
+                readOnly
+                onChange={handleChange}
+              />
+            </div>
+            <div className="gap-2 flex">
+              <label htmlFor="">Title</label>
+              <input
+                className="outline-1 rounded w-full h-fit font-bold text-lg px-2"
+                name="title"
+                type="text"
+                value={post.title}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="gap-2 flex">
+              <label htmlFor="">Body</label>
+              <input
+                className="outline-1 rounded w-full h-fit font-bold text-lg px-2"
+                name="body"
+                type="text"
+                value={post.body}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="gap-2 flex">
+              <input
+                className="outline-1 rounded bg-blue-700 text-white p-1 m-auto h-fit font-bold text-lg px-4"
+                type="submit"
+                value="Update"
+              />
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default UpdatePost;
